1ca1a431c9b3b9de39f70d3caaca7d63d763f9c3
Prezcot <janindumapa100@gmail.com>
2023-05-16 22:51:23 +0530

    init commit

---
+++ app/src/main/java/com/example/fortesting/l_course_work_creation.java
@@ -0,0 +1,13 @@
+package com.example.fortesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class l_course_work_creation extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.l_module_page);
+    }
+}
