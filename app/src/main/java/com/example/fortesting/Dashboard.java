49bd6839bc07f4993e920ab8020dd21d5690a559
Prezcot <janindumapa100@gmail.com>
2023-05-18 04:28:36 +0530

    stupid chami

--- app/src/main/java/com/example/fortesting/Dashboard.java
+++ app/src/main/java/com/example/fortesting/Dashboard.java
@@ -5,13 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -23,7 +20,7 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dashboard);
+        setContentView(R.layout.s_dashboard);
 
         Math = findViewById(R.id.math);
         View includedLayout = findViewById(R.id.nav_bar);
